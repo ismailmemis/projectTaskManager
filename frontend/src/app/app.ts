77db8c2f0fac1804db49864bca96b9af84e26d0e
Ismail Memis <i-memis@outlook.de>
2025-09-07 21:32:58 +0200

    create project

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { MenuModule } from 'primeng/menu';
 
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, MenuModule ],
--- frontend/src/components/create-project/create-project.ts
+++ frontend/src/components/create-project/create-project.ts
@@ -1,17 +1,41 @@
 import { Component } from '@angular/core';
 import { ProjectBase } from '../common/project-base/project-base';
 import { Project } from '../../api/models';
+import { ProjectService } from '../../api/services';
+import { Router } from '@angular/router';
+import { take } from 'rxjs';
+import { MessageService } from "primeng/api";
+import { ToastModule } from 'primeng/toast';
+
+
 
 @Component({
   selector: 'app-create-project',
-  imports: [ProjectBase],
+  imports: [ProjectBase, ToastModule],
   templateUrl: './create-project.html',
-  styleUrl: './create-project.scss'
+  styleUrl: './create-project.scss',
+  standalone: true,
 })
 export class CreateProject {
 
+
+  constructor(private readonly projectService: ProjectService, private readonly router: Router
+  ) { }
+
   onCreate(project: Project) {
     console.log('Neues Projekt erstellen:', project);
+    if (project.name && project.name.trim().length <= 0) {
+
+    }
+    this.projectService.createProject({ body: project }).pipe(take(1)).subscribe({
+      next: (response) => {
+        this.router.navigate(['/projects']);
+      },
+      error: (error) => {
+        console.error('Fehler beim Erstellen des Projekts:', error);
+        // Hier kann z.B. eine Fehlermeldung angezeigt werden
+      }
+    });
   }
 
   onCancel() {
